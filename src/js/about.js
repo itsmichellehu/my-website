125a62d071bc33af6e9168783261121b7ee27b62
mchll.h <michellehu0414@users.noreply.github.com>
2024-08-07 01:22:08 +0800

    fix: Temporarily remove ToolboxSwiper import in about.js to resolve Webpack bundling issues

--- src/js/about.js
+++ src/js/about.js
@@ -2,7 +2,6 @@ import '../scss/about.scss';
 
 // import '../components/NavBar/NavBar';
 import '../components/FaqAccordion/FaqAccordion';
-import '../components/ToolboxSwiper/ToolboxSwiper';
 import '../components/Scrollable/scrollable';
 import '../components/ScrollButton/scroll-button';
 
