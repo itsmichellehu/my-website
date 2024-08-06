0cd22aa4fe3f39c7899cef9841449af0a68b7e48
mchll.h <michellehu0414@users.noreply.github.com>
2024-08-07 01:34:28 +0800

    fix: Prevent Footer from being called twice on pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,31 @@
 function Footer() {
-    const footerHTML = `
-<footer>
-        <div class="wrapper">
-            <div class="text-wrapper">
-                <p class="tagline">Designed and coded with love by yours truly. 👩🏻‍💻 © 2024</p>
+    const existingFooter = document.querySelector('footer');
+    if (!existingFooter) {
+        const footerHTML = `
+        <footer>
+            <div class="wrapper">
+                <div class="text-wrapper">
+                    <p class="tagline">Designed and coded with love by yours truly. 👩🏻‍💻 © ${new Date().getFullYear()}</p>
+                </div>
+                <div class="cta-buttons hide">
+                    <button id="scrollToProjects-2" class="btn-primary scroll-button">See my work</button>
+                    <button class="btn-secondary">View my resume</button>
+                </div>
             </div>
-            <div class="cta-buttons hide">
-                <button id="scrollToProjects-2" class="btn-primary scroll-button">See my work</button>
-                <button class="btn-secondary">View my resume</button>
-            </div>
-        </div></footer>
-    `;
+        </footer>
+        `;
 
-    document.body.insertAdjacentHTML('beforeend', footerHTML);
+        document.body.insertAdjacentHTML('beforeend', footerHTML);
 
-    const scrollToProjectsButton = document.getElementById('scrollToProjects-2');
+        const scrollToProjectsButton = document.getElementById('scrollToProjects-2');
 
-    scrollToProjectsButton.addEventListener('click', () => {
-        const projectsSection = document.getElementById('projects');
-        if (projectsSection) {
-            projectsSection.scrollIntoView({ behavior: 'smooth' });
-        }
-    });
+        scrollToProjectsButton.addEventListener('click', () => {
+            const projectsSection = document.getElementById('projects');
+            if (projectsSection) {
+                projectsSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    }
 }
 
 export default Footer;
