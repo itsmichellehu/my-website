29fb5d11ab17ee9e36455dc30ff4baf11dce36c5
mchll.h <michellehu0414@users.noreply.github.com>
2024-08-07 02:37:14 +0800

    fix: Temporarily remove toggleMenu functions for NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -22,35 +22,35 @@ function NavBar() {
 
     // Ensure the classes are applied after the DOM is fully loaded and the HTML is inserted
     window.addEventListener('DOMContentLoaded', () => {
-        const menu = document.getElementById('menu');
-        const menuIcon = document.querySelector('.menu-icon');
-        const menuToggle = document.querySelector('.navbar-menu-toggle');
-
-        function toggleMenu() {
-            menu.classList.toggle('show-menu');
-        }
-
-        menuIcon.addEventListener('click', function (event) {
-            event.stopPropagation(); // Prevent document click event from closing the menu
-            toggleMenu();
-        });
-
-        document.addEventListener('click', function (event) {
-            if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
-                menu.classList.remove('show-menu');
-            }
-        });
-
-        menuToggle.addEventListener('click', function (event) {
-            event.stopPropagation(); // Prevent document click event from closing the menu
-            toggleMenu();
-        });
-
-        document.addEventListener('click', function (event) {
-            if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
-                menu.classList.remove('show-menu');
-            }
-        });
+        //         const menu = document.getElementById('menu');
+        //         const menuIcon = document.querySelector('.menu-icon');
+        //         const menuToggle = document.querySelector('.navbar-menu-toggle');
+        //
+        //         function toggleMenu() {
+        //             menu.classList.toggle('show-menu');
+        //         }
+        //
+        //         menuIcon.addEventListener('click', function (event) {
+        //             event.stopPropagation(); // Prevent document click event from closing the menu
+        //             toggleMenu();
+        //         });
+        //
+        //         document.addEventListener('click', function (event) {
+        //             if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
+        //                 menu.classList.remove('show-menu');
+        //             }
+        //         });
+        //
+        //         menuToggle.addEventListener('click', function (event) {
+        //             event.stopPropagation(); // Prevent document click event from closing the menu
+        //             toggleMenu();
+        //         });
+        //
+        //         document.addEventListener('click', function (event) {
+        //             if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+        //                 menu.classList.remove('show-menu');
+        //             }
+        //         });
 
         // Set active class based on current URL
         const navItems = document.querySelectorAll('.nav-item a');
