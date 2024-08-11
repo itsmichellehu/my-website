cfa3e7e034484facb1cc08fd938198c2fc7d32bf
mchll.h <michellehu0414@users.noreply.github.com>
2024-08-12 06:04:50 +0800

    wip: Create ImageZoom component

--- src/components/ImageZoom/ImageZoom.js
+++ src/components/ImageZoom/ImageZoom.js
@@ -1,8 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.img-zoomable').forEach(image => {
-        image.addEventListener('click', () => {
-            // Toggle the 'img-zoomed' class on click
-            image.classList.toggle('img-zoomed');
+import './_ImageZoom.scss';  // Import the CSS for zoom functionality
+
+function enableImageZoom() {
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelectorAll('.img-zoomable').forEach(image => {
+            image.addEventListener('click', () => {
+                // Toggle the 'img-zoomed' class on click
+                image.classList.toggle('img-zoomed');
+            });
         });
     });
-});
+}
+
+export default enableImageZoom;
