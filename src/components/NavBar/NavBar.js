9a5e8f432d6611fcd02b2c48c85a408b4a9d0e79
michellehu0414 <103789293+michellehu0414@users.noreply.github.com>
2024-09-11 01:42:00 -0700

    [index] feat: Implement new design for mobile

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,13 +2,15 @@ function NavBar() {
     const navbarHTML = `
         <nav id="navbar" class="navbar default-nav-color">
             <div id="navbar-wrapper" class="navbar-wrapper">
-                <div class="navbar-logo-wrapper"><a href="/"><img src="assets/svg/home/logo-mhu-black.svg" class="navbar-logo-image" alt="Logo" /></a>
+                <div class="navbar-logo-wrapper show-sm-flex"><a href="/"><img src="assets/svg/home/logo-mhu-black.svg" class="navbar-logo-image" alt="Logo" /></a>
 
                 </div>
                 <ul class="navbar-menu" id="menu">
                     <li class="nav-item"><a href="/">Work</a></li>
                     <li class="nav-item"><a href="/about">About</a></li>
-                    <li class="nav-item show-med-list-item"><a href="assets/files/MichelleHuResume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+                    <li class="nav-item"><a href="assets/files/MichelleHuResume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+</li>
+                    <li class="nav-item hide-sm"><a href="www.linkedin.com/in/itsmichellehu" target="_blank" rel="noopener noreferrer">LinkedIn</a>
 </li>
                     <li class="nav-item"><a href="mailto:mhu.contact@gmail.com">Contact</a></li>
                 </ul>
