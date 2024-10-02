cbc7c4aa7e585ce305ab7e8e24d9c483321d89fa
mchll.h <michellehu0414@users.noreply.github.com>
2024-10-02 05:55:51 -0700

    style(_hero-header & _section-overview): Make hero and overview sections responsive

--- src/components/ScrollToChallenge/ScrollToChallenge.js
+++ src/components/ScrollToChallenge/ScrollToChallenge.js
@@ -2,7 +2,6 @@ function scrollToChallenge() {
     if (window.innerWidth < 768) {  // Check if the screen width is less than 768px
         document.getElementById('scrollToChallenge').addEventListener('click', function () {
             const target = document.getElementById('the-challenge');
-            const defaultOffset = 3 * 16; // Convert 3.5rem to pixels (assuming the root font-size is 16px)
 
             const offset = defaultOffset;
             const bodyRect = document.body.getBoundingClientRect().top;
