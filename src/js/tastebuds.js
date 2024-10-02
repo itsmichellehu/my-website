5064985d18d9466e1e2df372717ea92c6be20628
mchll.h <michellehu0414@users.noreply.github.com>
2024-10-02 09:08:18 -0700

    fix(tastebuds.js): Revert today's changes to resolve component import issues

--- src/js/tastebuds.js
+++ src/js/tastebuds.js
@@ -1,43 +1,48 @@
+// Import SCSS file for this page
 import '../scss/tastebuds.scss';
 
-// Import components
+// Import and initialize components specific to this page
+// import '../components/Carousel/carousel';
 import '../components/AutoplayVideo/autoplay-video';
-import '../components/AutoplayVideoCarousel/AutoplayVideoCarousel';
+import '../components/AutoplayVideoCarousel/AutoplayVideoCarousel'
 import '../components/ImageZoom/ImageZoom';
 import '../components/ScrollButton/scroll-button';
 import '../components/SequentialVideoPlayer/sequential-video-player';
-import '../components/Scrollable/scrollable';
 
 import MediaCarousel from '../components/MediaCarousel/MediaCarousel';
+document.addEventListener('DOMContentLoaded', () => {
+    // Assuming you have two carousels with IDs `carousel-01` and `carousel-02`
+    const carousel1 = new MediaCarousel('#carousel-01');
+    const carousel2 = new MediaCarousel('#carousel-02');
+    const carousel3 = new MediaCarousel('#carousel-03');
+
+    // If you have more carousels, continue creating instances similarly
+    // const carousel3 = new MediaCarousel('#carousel-03');
+});
+
+import '../components/Scrollable/scrollable';
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Main TasteBuds page loaded');
+    // Additional initialization code for this page
+});
+
 import NavBar from '../components/NavBar/NavBar';
 import NavBarBackground from '../components/NavBarBackground/NavBarBackground';
 import ProgressBar from '../components/ProjectProgressBar/ProjectProgressBar';
 import BackToTopButton from '../components/BackToTopButton/BackToTopButton';
 import Footer from '../components/Footer/Footer';
-import ScrollToSection from '../components/ScrollToASection/ScrollToASection';
+
 import { initializeAccordion } from '../components/ProjectAccordion/ProjectAccordion';
 import { initializeHideVideoControls } from '../components/HideVideoControls/HideVideoControls';
 
-// Initialize page elements once DOM is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Main TasteBuds page loaded');
-
-    // Initialize Carousels
-    const carousel1 = new MediaCarousel('#carousel-01');
-    const carousel2 = new MediaCarousel('#carousel-02');
-    const carousel3 = new MediaCarousel('#carousel-03');
-
-    // Initialize Navbar and other components
     NavBar();
     NavBarBackground();
     ProgressBar();
     BackToTopButton();
     Footer();
 
-    // Initialize other page-specific components
     initializeAccordion();
-    ScrollToSection();
     initializeHideVideoControls();
-
-    console.log("TasteBuds Page Initialized!");
 });
