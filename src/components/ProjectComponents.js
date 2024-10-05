76696a0b9a8e1175852b34c76a032df91c03bfae
mchll.h <michellehu0414@users.noreply.github.com>
2024-10-05 03:26:38 -0700

    refactor: Rename Tabs to TabsContainer to fix deployment failure
    - Renamed the `Tabs` component to `TabsContainer` to resolve deployment issues where the component could not be located.
    - Updated all relevant imports and references to reflect the new component name.
    - This refactor ensures consistent naming and avoids further deployment issues.

--- src/components/ProjectComponents.js
+++ src/components/ProjectComponents.js
@@ -2,7 +2,7 @@
 import ProgressBar from '../components/ProjectProgressBar/ProjectProgressBar';
 
 import { initializeAccordion } from '../components/ProjectAccordion/ProjectAccordion';
-import initializeTabs from '../components/Tabs/Tabs';
+import initializeTabs from '../components/TabsContainer/TabsContainer';
 import enableImageZoom from '../components/ImageZoom/ImageZoom';
 
 document.addEventListener('DOMContentLoaded', function () {
