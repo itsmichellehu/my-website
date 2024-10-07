f8025aae3d5564cfcb4d44005dbf74d3a0917dfa
mchll.h <michellehu0414@users.noreply.github.com>
2024-10-07 06:01:06 -0700

    chore(ProjectComponents.js): Comment out ProjectAccordion

--- src/components/ProjectComponents.js
+++ src/components/ProjectComponents.js
@@ -1,14 +1,14 @@
 
 import ProgressBar from '../components/ProjectProgressBar/ProjectProgressBar';
 
-import { initializeAccordion } from '../components/ProjectAccordion/ProjectAccordion';
+// import { initializeAccordion } from '../components/ProjectAccordion/ProjectAccordion';
 import initializeTabs from '../components/TabsContainer/TabsContainer';
 import enableImageZoom from '../components/ImageZoom/ImageZoom';
 
 document.addEventListener('DOMContentLoaded', function () {
     ProgressBar();
 
-    initializeAccordion();
+    // initializeAccordion();
     enableImageZoom();
     initializeTabs();
 });
