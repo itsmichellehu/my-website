e45c845c3d125b841706466b3d3b51127eff1f9f
mchll.h <michellehu0414@users.noreply.github.com>
2024-10-13 15:27:07 -0700

    refactor(about.js): Import GlobalComponents.js and remove duplicates

--- src/js/about.js
+++ src/js/about.js
@@ -1,19 +1,17 @@
 import '../scss/about.scss';
 
-// import '../components/NavBar/NavBar';
+import '../components/GlobalComponents';
 import { initializeFaqAccordion } from "../components/FaqAccordion/FaqAccordion";
-import '../components/Scrollable/scrollable';
-import '../components/ScrollButton/scroll-button';
-
-import NavBar from '../components/NavBar/NavBar';
-import BackToTopButton from '../components/BackToTopButton/BackToTopButton';
-import Footer from '../components/Footer/Footer';
 import ProgressBar from '../components/ProjectProgressBar/ProjectProgressBar';
+import '../components/Scrollable/scrollable';
+
+import BackToTopButton from '../components/BackToTopButton/BackToTopButton';
 
 document.addEventListener('DOMContentLoaded', () => {
     BackToTopButton();
     Footer();
     NavBar();
     ProgressBar();
+
     initializeFaqAccordion();
 });
