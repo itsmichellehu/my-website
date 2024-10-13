54690bdcf3f0245134b4e398cda2cbcf3c00e098
mchll.h <michellehu0414@users.noreply.github.com>
2024-10-13 05:16:31 -0700

    chore(NavBarBackground): Remove unused viewportWidth variable

--- src/components/NavBarBackground/NavBarBackground.js
+++ src/components/NavBarBackground/NavBarBackground.js
@@ -3,8 +3,6 @@ function NavBarBackground() {
         const navbar = document.getElementById('navbar');
         const sections = document.querySelectorAll('section');
         let currentSection = '';
-        const viewportWidth = window.innerWidth;
-
         sections.forEach(section => {
             const sectionTop = section.offsetTop;
             const sectionHeight = section.clientHeight;
