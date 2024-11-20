22a099becbe222aa6adf5c45dae237caaefbbb24
michellehu0414 <103789293+michellehu0414@users.noreply.github.com>
2024-11-20 08:00:37 -0800

    feat: Comment out carousel import in tastebuds.js

--- src/js/tastebuds.js
+++ src/js/tastebuds.js
@@ -2,7 +2,7 @@ import '../scss/tastebuds.scss';
 import '../components/GlobalComponents';
 import '../components/ProjectComponents';
 
-import '../components/Carousel/carousel';
+// import '../components/Carousel/carousel';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Main TasteBuds page loaded');
