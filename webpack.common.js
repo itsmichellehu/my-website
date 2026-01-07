784bbe029c78ce28d99c41174f36cbed8967c1a1
mchll.h <hump1@uci.edu>
2026-01-07 04:14:00 -0800

    fix(webpack): enhance entry configuration and resolve paths for improved module loading

--- webpack.common.js
+++ webpack.common.js
@@ -3,35 +3,40 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const pages = ["index", "about", "boardspace", "postup", "tastebuds"];
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-	entry: {
-		index: "./src/js/index.js",
-		about: "./src/js/about.js",
-		boardspace: "./src/js/boardspace.js",
-		postup: "./src/js/postup.js",
-		tastebuds: "./src/js/tastebuds.js"
+	resolve: {
+		extensions: [".js"],
+		roots: [path.resolve("./src")],
+		alias: {
+			"@scss": path.resolve(__dirname, "src/scss"),
+			"@components": path.resolve(__dirname, "src/components")
+		}
 	},
+
+	entry: pages.reduce((entries, page) => {
+		entries[page] = `./src/js/${page}.js`;
+		return entries;
+	}, {}),
 
 	output: {
 		path: path.resolve(__dirname, "dist"),
-		clean: true,
-		assetModuleFilename: "assets/[name].[contenthash:8][ext][query]"
-	},
-
-	resolve: {
-		extensions: ["", ".js"],
-		root: [path.resolve("./src")],
+		filename: "js/[name].[contenthash:8].js",
+		assetModuleFilename: "assets/[name].[contenthash:8][ext][query]",
+		clean: true
 	},
 
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
-				loader: "babel-loader",
 				exclude: /node_modules/,
-				options: {
-					presets: ["@babel/preset-env"]
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
 				}
 			},
 
@@ -50,22 +55,28 @@ module.exports = {
 					filename: "assets/videos/[name].[contenthash:8][ext]"
 				}
 			},
+
 			{
 				test: /\.(scss|css)$/,
 				use: [
-					process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
+					isProduction ? MiniCssExtractPlugin.loader : "style-loader",
 					"css-loader",
 					{
 						loader: "sass-loader",
 						options: {
 							implementation: require("sass"),
 							sassOptions: {
-								includePaths: [path.join(__dirname, "src")]
+								includePaths: [
+									path.join(__dirname, "src"),
+									path.resolve(__dirname, "src/scss"),
+									path.join(__dirname, "src/components")
+								]
 							}
 						}
 					}
 				]
 			},
+
 			{
 				test: /\.(woff2?|ttf|eot|otf)$/i,
 				type: "asset/resource",
