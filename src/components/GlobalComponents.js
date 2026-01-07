34d980b40f0db216de4d3627f224f22a5f3a0b30
mchll.h <hump1@uci.edu>
2026-01-06 19:37:39 -0800

    chore(GlobalComponents): Remove unused AnimateInView component

--- src/components/GlobalComponents.js
+++ src/components/GlobalComponents.js
@@ -1,25 +1,14 @@
-import NavBar from '../components/NavBar/NavBar';
-import NavBarBackground from '../components/NavBarBackground/NavBarBackground';
-import Footer from '../components/Footer/Footer';
+import NavBar from "./NavBar/NavBar";
+import NavBarBackground from "./NavBarBackground/NavBarBackground";
+import Footer from "./Footer/Footer";
 
-import ScrollToSection from './ScrollToSection/ScrollToSection';
-import { initializeHideVideoControls } from '../components/HideVideoControls/HideVideoControls';
+import ScrollToSection from "./ScrollToSection/ScrollToSection";
+import { initializeHideVideoControls } from "./HideVideoControls/HideVideoControls";
 
-import { AnimateInView } from "../components/AnimateInView/AnimateInView";
-const classAnimationMap = {
-    'fadeIn_inView': 'animate__fadeIn',
-    'fadeInRight_inView': 'animate__fadeInRight',
-    'fadeInLeft_inView': 'animate__fadeInLeft',
-    'slideInUp_inView': 'animate__slideInUp',
-    'bounceIn_inView': 'animate__bounceIn'
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-    NavBar();
-    NavBarBackground();
-    Footer();
-
-    AnimateInView({ classAnimationMap });
-    ScrollToSection();
-    initializeHideVideoControls();
+document.addEventListener("DOMContentLoaded", () => {
+	NavBar();
+	NavBarBackground();
+	Footer();
+	ScrollToSection();
+	initializeHideVideoControls();
 });
