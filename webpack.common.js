51fbfcac62f876cea0da0338c89799d42d7d9834
mchll.h <hump1@uci.edu>
2026-06-08 17:49:09 -0700

    build(webpack): inject NODE_ENV via DefinePlugin
    Add webpack.DefinePlugin to define process.env.NODE_ENV (defaulting to
    "development") so it's available to client bundles. Reformat file to
    2-space indentation.

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -8,111 +9,117 @@ const pages = ["index", "about", "postup", "tastebuds"];
 const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-	resolve: {
-		extensions: [".js", ".json"],
-		alias: {
-			"@": path.resolve(__dirname, "src/"),
-			"@assets": path.resolve(__dirname, "src/assets"),
-			"@js": path.resolve(__dirname, "src/js"),
-			"@components": path.resolve(__dirname, "src/components"),
-			"@scss": path.resolve(__dirname, "src/scss"),
-			"@abstracts": path.resolve(__dirname, "src/scss/abstracts"),
-			"@base": path.resolve(__dirname, "src/scss/base"),
-			"@sections": path.resolve(__dirname, "src/scss/sections"),
-			"@utilities": path.resolve(__dirname, "src/scss/utilities"),
-			"@pages": path.resolve(__dirname, "src/scss/pages"),
-		},
-	},
+  resolve: {
+    extensions: [".js", ".json"],
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@js": path.resolve(__dirname, "src/js"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@scss": path.resolve(__dirname, "src/scss"),
+      "@abstracts": path.resolve(__dirname, "src/scss/abstracts"),
+      "@base": path.resolve(__dirname, "src/scss/base"),
+      "@sections": path.resolve(__dirname, "src/scss/sections"),
+      "@utilities": path.resolve(__dirname, "src/scss/utilities"),
+      "@pages": path.resolve(__dirname, "src/scss/pages"),
+    },
+  },
 
-	entry: {
-		main: "./src/js/main.js",
-	},
+  entry: {
+    main: "./src/js/main.js",
+  },
 
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "js/[name].js",
-		assetModuleFilename: "assets/[name].[ext][query]",
-		clean: true,
-		publicPath: "/",
-	},
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+    assetModuleFilename: "assets/[name].[ext][query]",
+    clean: true,
+    publicPath: "/",
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env"],
-					},
-				},
-			},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
 
-			{
-				test: /\.(png|jpe?g|gif|webp)$/i,
-				type: "asset",
-				parser: {
-					dataUrlCondition: { maxSize: 4 * 1024 },
-				},
-			},
-			{
-				test: /\.svg$/i,
-				type: "asset/resource",
-			},
-			{
-				test: /\.(mp4|webm)$/i,
-				type: "asset/resource",
-				generator: {
-					filename: "assets/videos/[name].[contenthash:8][ext]",
-				},
-			},
-			{
-				test: /\.(scss|css)$/,
-				use: [
-					isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-					"css-loader",
-					{
-						loader: "sass-loader",
-						options: {
-							implementation: require("sass"),
-							sassOptions: {
-								includePaths: [path.resolve(__dirname, "../src/scss")],
-							},
-						},
-					},
-				],
-			},
-			{
-				test: /\.(woff2?|ttf|eot|otf)$/i,
-				type: "asset/resource",
-				generator: {
-					filename: "assets/fonts/[name].[contenthash:8][ext]",
-				},
-			},
-		],
-	},
+      {
+        test: /\.(png|jpe?g|gif|webp)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: { maxSize: 4 * 1024 },
+        },
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(mp4|webm)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/videos/[name].[contenthash:8][ext]",
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+              sassOptions: {
+                includePaths: [path.resolve(__dirname, "../src/scss")],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2?|ttf|eot|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[name].[contenthash:8][ext]",
+        },
+      },
+    ],
+  },
 
-	plugins: [
-		...pages.map(
-			(page) =>
-				new HtmlWebpackPlugin({
-					template: `./src/${page}.html`,
-					filename: `${page}.html`,
-					chunks: ["main"], // main.js on every page
-					inject: "head",
-					scriptLoading: "defer",
-				})
-		),
+  plugins: [
+    ...pages.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: `./src/${page}.html`,
+          filename: `${page}.html`,
+          chunks: ["main"], // main.js on every page
+          inject: "head",
+          scriptLoading: "defer",
+        }),
+    ),
 
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: path.resolve(__dirname, "src/assets"),
-					to: "assets",
-					noErrorOnMissing: true,
-				},
-			],
-		}),
-	],
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets"),
+          to: "assets",
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development",
+      ),
+    }),
+  ],
 };
